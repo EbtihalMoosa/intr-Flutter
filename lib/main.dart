9475dcadfbd4d6a02c5e928f861ba9622c8deae0
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-09-20 23:14:32 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+void main(
+) {
+  runApp(Myapp());
+  
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: FirstBage(),);
+  }
+}
+
+
+class FirstBage extends StatelessWidget {
+  const FirstBage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+
+    
+
+
+  
+// body: Column(
+//   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//   crossAxisAlignment: CrossAxisAlignment.start,
+
+//   children: [
+//    Row(
+//   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+//   Container(
+//       width: 200,
+//       height: 200,
+//       alignment: Alignment.center,
+//       child: Text ("Image 1"),
+//       decoration: BoxDecoration(image: DecorationImage(image: AssetImage(".dart_tool/assets/image.jpg"),
+//       fit: BoxFit.cover)),
+    
+//     ),
+//     Container(
+//       width: 200,
+//       height: 200,
+//       alignment: Alignment.center,
+//       child: Text ("Image 1"),
+//       decoration: BoxDecoration(image: DecorationImage(image: AssetImage(".dart_tool/assets/image.jpg"),
+//       fit: BoxFit.cover)),
+    
+//     ),
+// ],
+//    ),
+  
+//     Container(child: Text("First APP"),),
+
+//     Container(
+//       width: 200,
+//       height: 200,
+//     alignment: Alignment.center,
+//       child: Text ("Image 2"),
+//       decoration: BoxDecoration(image: DecorationImage(image: AssetImage(".dart_tool/assets/image.jpg"),
+//       fit: BoxFit.cover)),
+    
+    
+//     ),
+    
+    
+// ]),
+
+// ),
+    
+                
+    
+//     body: Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//      Container(
+//       width: 500,
+//       height: 240,
+//       alignment: Alignment.center,
+//       decoration: BoxDecoration(image: DecorationImage(image: AssetImage(".dart_tool/assets/image.jpg"),
+//       fit: BoxFit.cover)),
+//      ),
+
+//     Container(child: Text("Name: Ebtihal",style: TextStyle(
+//     fontFamily: "Schyler" ),),
+//     margin: EdgeInsets.only(left: 20, top: 20),
+//     alignment: Alignment.centerLeft,),
+//     Container(child: Text("Phone: +968 99273637",style: TextStyle(
+//       fontFamily: "Schyler"),),
+//       margin: EdgeInsets.only(left: 20),
+//       alignment: Alignment.centerLeft,),
+//     Container(child: Text("Email: Ebtihall@gmail.com",style: TextStyle(
+//       fontFamily: "Schyler"),),
+//       alignment: Alignment.centerLeft,
+//       margin: EdgeInsets.only(left: 20, bottom: 120),),
+
+// Column(
+// // mainAxisAlignment: MainAxisAlignment.end,
+
+// children: [
+//   Container(
+//   alignment: Alignment.center,
+//   decoration:BoxDecoration(color: Color.fromRGBO(48, 134, 78, 1),
+//    borderRadius: BorderRadius.circular(9),
+//    ),
+//    width: 450,
+//    height: 30,
+//   margin: EdgeInsets.all(10),
+//   child:   Text ("Call Him", style: TextStyle(
+//     color: Colors.white,
+//     // fontSize: 15
+//   ),),
+  
+
+
+//  Container(
+//   width: 450,
+//   height: 30,
+//   alignment: Alignment.center,
+//   decoration:BoxDecoration(color: Color.fromRGBO(208, 28, 28, 1),
+//    borderRadius: BorderRadius.circular(9),
+//    ),
+//   margin: EdgeInsets.all(10),
+//   child:   Text ("Add To Contacts", style: TextStyle(
+//     color: Colors.white,
+//     // fontSize: 15
+//   ),),
+  
+// )
+
+
+// ],
+// )
+
+//     ],)
+    
+    
+//     ),
+    
+    
+    
+            
+    
+
+//     drawer: Drawer(backgroundColor: Colors.black,
+//     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+//     width: 100,
+//     child: CircleAvatar(radius: 50),),
+
+    // appBar: AppBar(
+
+    //    actions: [Icon(Icons.search),Icon(Icons.shopping_cart),Icon(Icons.settings)],
+    //    centerTitle: true,
+    //    title: Text("First App"),
+
+    //   backgroundColor: Colors.teal,
+    //   // leading: Icon(Icons.arrow_back_ios_new, 
+    //   // color:Colors.white,
+    //   // size: 20,),),
+    // ),
+// //Circle Avatar
+//       body: CircleAvatar(
+//         radius: 70,
+//         // backgroundColor: Colors.amber,
+//         backgroundImage: AssetImage(".dart_tool/assets/image.jpg"),
+//       ),
+//Image
+     // body: Image.network("https://c4.wallpaperflare.com/wallpaper/740/111/798/ocean-background-wallpaper-preview.jpg",width: 100,height: 100),
+
+    // child: Image.asset(".dart_tool/assets/image.jpg"))),
+// body: Container(
+//   decoration:BoxDecoration(color: Color.fromARGB(255, 185, 241, 205),
+//    borderRadius: BorderRadius.circular(16),
+//    boxShadow:[BoxShadow(color: Colors.black, offset: Offset(1, 2))]),
+//   margin: EdgeInsets.only(top: 150, left:160, right:20, bottom: 20),
+//   child:   Text ("Hello World",
+  
+//   style: TextStyle(
+//     color: Colors.green,
+//     fontSize: 25,
+//     wordSpacing: 17,
+//     letterSpacing: 5,
+//     fontWeight: FontWeight.w300,
+//     fontFamily: "Schyler"
+  
+  
+  
+//   ),
+  
+  
+//      ),
+// ) 
+    
+  
